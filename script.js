da5510b72ede637d6bfce89896dbc44512c6df48
Hafifahussein <http://hafifahussein5gmail.com>
2025-06-25 00:04:39 +0300

    API Configuration initialize the API key, defined the BaseURL and forecast data

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const apiKey = '070870365c920bd47d550f0447337663';
+    const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+    const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+
+});
